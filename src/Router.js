d27a0b754192c5d920d681ade2d13d75dbbe075d
Caroline-Patrick <carolinespatrick@gmail.com>
2023-02-26 18:28:21 -0600

    added containers and state.js reducer info to make redux functional

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { Switch, Route } from 'react-router'
-import Home from './components/Home'
+import { Routes, Route, Navigate } from 'react-router'
+import Home from './containers/Home'
 import About from './components/About'
-import Car from './components/Car'
-import Dashboard from './components/Dashboard'
+import Car from './containers/Car'
+import Dashboard from './containers/Dashboard'
 
 const Router = () => {
     return (
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/car/:id" component={Car} />
-            <Route path="/dashboard" component={Dashboard} />
-        </Switch>
+        <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/car/:id" element={<Car/>} />
+            <Route path="/dashboard" element={<Dashboard/>} />
+        </Routes>
     );
 };
 
